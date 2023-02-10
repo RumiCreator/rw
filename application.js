9d032696c81fcaca4cc6f6c9da209acb5da0f07c
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-02-09 21:18:28 -0800

    Update application.js

--- application.js
+++ application.js
@@ -18,6 +18,8 @@
   GUI.style.borderRadius = "6px"
   GUI.style.height = "155px";
   GUI.style.width = "160px";
+  GUI.style.boxShadow = "0 16px 32px 0 rgba(0,0,0,0.2), 0 12px 40px 0 rgba(0,0,0,0.19)";
+
 
   GUI.style.zIndex = 2147483646;
   document.body.appendChild(GUI);
